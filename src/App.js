68e7ce11994a00607807c2692397edc0a620eb52
cmacleod <45601392+Dobly1@users.noreply.github.com>
2023-03-03 20:54:17 -0500

    Refactor App to allow multiple pages

--- src/App.js
+++ src/App.js
@@ -1,36 +1,39 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import React from 'react'
-import {
-  Container,
-  Navbar,
-  Nav
-} from 'react-bootstrap'
+import {Container, Navbar, Nav, NavDropdown } from 'react-bootstrap'
 import CustomerCard from './Customer/Customer';
 
 function App() {
-  return (
-    <div>
-      <Header/>
-      <CustomerCard/>
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <CustomerCard/>
+        </div>
+    );
 }
 
 function Header() {
-  return (
+return (
     <Navbar>
-      <Container>
-        <Navbar.Brand href="#home">POS</Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#transactions">Transactions</Nav.Link>
-          <Nav.Link href="#customers">Customers</Nav.Link>
-        </Nav>
-      </Container>
+        <Container>
+            <Navbar.Brand href="#home">POS</Navbar.Brand>
+                <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <NavDropdown title="Transactions" id="TransactionDropDown">
+                    <NavDropdown.Item>Holder</NavDropdown.Item>
+                    <NavDropdown.Item>Holder</NavDropdown.Item>
+                    <NavDropdown.Item>Holder</NavDropdown.Item>
+                </NavDropdown>
+                <NavDropdown title="Customers" id="CustomerDropDown">
+                    <NavDropdown.Item>Search</NavDropdown.Item>
+                    <NavDropdown.Item>Create</NavDropdown.Item>
+                </NavDropdown>
+            </Nav>
+        </Container>
     </Navbar>
-  );
+);
 }
+
 
 export default App;
---
+++ src/Customer/CustomerFunctions.js
@@ -0,0 +1,15 @@
+import {useState,useEffect,React} from 'react';
+import axios from 'axios';
+import Card from 'react-bootstrap/Card';
+import Table from 'react-bootstrap/Table'
+import Button  from 'react-bootstrap/Button';
+import { Container, Row, Col} from 'react-bootstrap';
+
+
+function SearchCustomer(props) {
+
+}
+
+function AddCustomer(props) {
+
+}
